2bfb7841cdc687f3bd5e624225e24565833b3e2d
Nitesh Singh <103584809+niteshsingh12122003@users.noreply.github.com>
2023-02-19 23:58:50 +0530

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+// int ElementSearch(vector<int> v,int key){
+//     int s=0;int e=v.size()-1;int m=s+(e-s)/2;
+//     while(s<=e){
+//         if(key==v[m]){
+//             return m;
+//         }
+//      else if(key>v[m]){
+//         s=m+1;
+//      }
+//      else{
+//         e=m-1;
+//      }
+//      m=s+(e-s)/2;
+//     }
+//     return -1;
+// }
+int main(){
+//     vector<int> v{1,4,6,10,15,18};
+// // int key=12;
+// //  cout<<ElementSearch(v,key);
+// if(binary_search(v.begin(),v.end(),001111111111)){
+// cout<<"True"<<endl;
+// }else{
+//     cout<<"False"<<endl;
+// }
+// int arr[]={1,3,4,6,8,9};
+// int size=6;
+// if(binary_search(arr,arr+size,3.0001)){
+//     cout<<"T"<<endl;
+// }else{
+//     cout<<"F"<<endl;
+// }
+}
